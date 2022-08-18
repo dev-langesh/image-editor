12dff4d6221da3974b0aa218a07428dcc7f9de3f
dev-langesh <dev.langesh@gmail.com>
2022-08-18 19:25:24 +0530

    final

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useAppDispatch } from "../src/app/hooks";
 import { reset } from "../src/features/filters/filters";
 import styles from "../styles/Footer.module.css";
 
 export default function Footer() {
   const dispatch = useAppDispatch();
+  const inputRef = useRef<HTMLInputElement>();
+
+  function chooseImage() {
+    inputRef.current?
+  }
+
   return (
     <footer className={styles.footer}>
       <button onClick={() => dispatch(reset())} className={styles.resetBtn}>
         Reset
       </button>
+
+      <input
+        ref={inputRef}
+        type="file"
+        name="image"
+        className={styles.inputImage}
+      />
 
       <div className={styles.btnGroup}>
         <button className={styles.chooseBtn}>Choose</button>
